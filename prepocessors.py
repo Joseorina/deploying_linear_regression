60a9e98634ea491bc3f02045324a46da0d85d416
Joseorina <orinangare@gmail.com>
2019-12-16 04:47:04 +0300

    [Training the model]

--- prepocessors.py
+++ prepocessors.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
 
 class CategoricalImputer(BaseEstimator, TransformerMixin):
@@ -30,4 +31,26 @@ class CategoricalImputer(BaseEstimator, TransformerMixin):
             X[feature] =X[feature].fillna('Missing')
         return X            
         
-        
+class NumericalImputer(BaseEstimator, TransformerMixin):
+    """
+    Numerical missing values imputer
+    """
+    def __init__(self, variables=None):
+        if not isinstance(variables, list):
+            self.variables = [variables]
+        else:
+            self.variables = variables
+            
+    def fit(self, X, y=None):
+        # persist mode in dictionary
+        self.imputer_dict_ = {}
+        for feature in self.variables:
+            self.imputer_dict_[feature] = X[feature].mode()[0]
+        return self
+    
+    def transform(self, X):
+        X = X.copy()
+        for feature in self.variables:
+            X[feature].fillna(self.imputer_dict_[feature], inpace=True)
+        return X
+                            
