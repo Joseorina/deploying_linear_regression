f3bb94ba426785c7decffa3e36c25c45b62a687f
Joseorina <orinangare@gmail.com>
2019-12-16 04:12:21 +0300

    [Training the model]

--- train_pipeline.py
+++ train_pipeline.py
@@ -7,3 +7,24 @@ DATASET_DIR = PACKAGE_ROOT / 'datasets'
 TESTING_DATA_FILE = DATASET_DIR / 'test.csv'
 TRAINING_DATA_FILE = DATASET_DIR / 'train.csv'
 TARGET = 'SalePrice'
+
+FEATURES = ['MSSubClass', 'MSZoning', 'Neighborhood', 'OverallQual',
+            'OverallCond', 'YearRemodAdd', 'RoofStyle', 'MasVnrType',
+            'BsmtQual', 'BsmtExposure', 'HeatingQC', 'CentralAir',
+            '1stFlrSF', 'GrLivArea', 'BsmtFullBath', 'KitchenQual',
+            'Fireplaces', 'FireplaceQu', 'GarageType', 'GarageFinish',
+            'GarageCars', 'PavedDrive', 'LotFrontage',
+            # this variable is only to calculate temporal variable:
+            'YrSold']
+
+def save_pipeline() -> None:
+    """
+    persist the pipeline
+    """
+    pass
+
+def run_training() -> None:
+    print('Training ... ')
+    
+if __name__ == "__main__":
+    run_training()    
