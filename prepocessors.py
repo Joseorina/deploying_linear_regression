c6227e26213c151681a4947ce55bfb2476a2cabc
Joseorina <orinangare@gmail.com>
2019-12-16 07:18:22 +0300

    [Training the model]

--- prepocessors.py
+++ prepocessors.py
@@ -55,3 +55,41 @@ class NumericalImputer(BaseEstimator, TransformerMixin):
         for feature in self.variables:
             X[feature].fillna(self.imputer_dict_[feature], inpace=True)
         return X
+
+class TempralVariablesEstimator(self, X):
+    def __init__(self, variables=None, reference_variable=None):
+        if not isinstance(variables, list):
+            self.variables = [variables]
+        else:
+            self.variables = variables
+    
+    def fit(self, X, y= None):
+        # we need this tep to fit to sklearn pipeline
+        return self
+    
+    def transform(self, X):
+        X = X.copy()
+        for feature in self.variables:
+            X[feature] = X[self.reference_variables] - X[feature]
+        return X
+
+class RareLabelCategoricalEncoder(BaseEstimator, TransformerMixin):
+    """
+    rare label categorical encoder
+    """
+    def __init__(self, tol=0.05, variables=None):
+        self.tol = tol
+        if not isinstance(variables, list):
+            self.variables = [variables]
+        else:
+            self.variables = variables
+    
+    def fit(self, X, y=None):
+        # persist frequent labels in dictionary
+        self.encoder_dict_ = {}
+        
+        for var in self.variables:
+            # the encoder will learn the most frequent categories
+            t = pd.Series(X[var].value_counts() / np.float(len(X)))
+            # frequent labels
+            self.encoder_dict_[var] = list(t[t >= self.tol].index)                                                            
